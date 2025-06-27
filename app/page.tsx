4d4ad7b3a95fdc7ceea5da6221a0f2b54cef401b
Bagg <Cameronwatkins46@gmail.com>
2025-06-27 16:17:57 -0500

    implement navbar

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import {Button} from "@/components/ui/button";
+
+const Page = () => {
+    return (
+        <div>
+            <h1 className="text-2xl underline">Welcome to my SaaS App</h1>
+            <Button>
+                {/* eslint-disable-next-line */}
+                Let's get started
+            </Button>
+        </div>
+    )
+}
+
+export default Page
