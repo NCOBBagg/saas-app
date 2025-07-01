f193c901a6f7ea7f4f8976ae10584b1b4acf20ec
Bagg <Cameronwatkins46@gmail.com>
2025-07-01 06:29:52 -0500

    implement companion card styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,44 @@
-import {Button} from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 
 const Page = () => {
     return (
-        <div>
-            <h1 className="text-2xl underline">Welcome to my SaaS App</h1>
-            <Button>
-                {/* eslint-disable-next-line */}
-                Let's get started
-            </Button>
-        </div>
+        <main>
+            <h1 className="text-2xl underline">Popular Companion</h1>
+
+            <section className="home-section">
+                <CompanionCard
+                    id="123"
+                    name="Neura the Brainy Explorer"
+                    topic="Neaural Network of the Brain"
+                    subject="Science"
+                    duration={45}
+                    color="#ffda6e"
+                />
+                <CompanionCard
+                    id="456"
+                    name="Counsty the Number Wizard"
+                    topic="Derivatives & Integrals"
+                    subject="Math"
+                    duration={30}
+                    color="#e5d0ff"
+                />
+                <CompanionCard
+                    id="789"
+                    name="Verba the Vocabulary Builder"
+                    topic="English Literature"
+                    subject="Language"
+                    duration={30}
+                    color="#BDE7FF"
+                />
+            </section>
+
+            <section className="home-section">
+                <CompanionsList/>
+                <CTA/>
+            </section>
+        </main>
     )
 }
 
